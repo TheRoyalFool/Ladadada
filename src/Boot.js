9e0afec48ef9a8ce6c4650c233869fe306a4c0bb
kipppys <themousesack@hotmail.co.uk>
2014-10-30 00:39:58 +0000

    update 1

--- src/Boot.js
+++ src/Boot.js
@@ -1,3 +1,16 @@
-/**
- * Created by kipppys on 30/10/14.
- */
+window.onload = function() {
+
+    var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+
+    function preload() {
+
+    }
+
+    function create() {
+
+    }
+
+    function update() {
+
+    }
+}
