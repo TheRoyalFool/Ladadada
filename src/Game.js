72335ed6798cf710ac65794b2a65a02f81a694cf
kipppys <themousesack@hotmail.co.uk>
2014-10-31 20:02:35 +0000

    update 2

--- src/Game.js
+++ src/Game.js
@@ -1,3 +1,23 @@
-/**
- * Created by kipppys on 30/10/14.
- */
+window.onload = function(){
+
+    var game = new Phaser.Game(1200,800,Phaser.AUTO,'container',
+        {preload: preload, create: create, update: update});
+
+    var player;
+
+    function preload(){
+        game.load.image('playerimg','assets/player.PNG');
+    }
+
+    function create(){
+        player = new Player(game, 100, 100, 'playerimg', 1, 10, 0.1);
+        game.add.existing(player);
+    }
+
+    function update(){
+
+
+
+    }
+
+}
--- src/Objects/Player.js
+++ src/Objects/Player.js
@@ -1,5 +1,33 @@
-Player = function(game, x, y, img){
+Player = function(game, x, y, img, speed, maxSpeed, acceleration){
     Phaser.Sprite.call(this, game, x, y, img);
+    this.playerImg = img;
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
+    this.maxSpeed = maxSpeed;
+    this.acceleration = acceleration;
+    this.v = 0;
+}
 
+Player.prototype = Object.create(Phaser.Sprite.prototype);
+Player.prototype.constructor = Player;
 
+Player.prototype.update = function(){
+
+   if(this.game.input.keyboard.isDown(Phaser.Keyboard.A)){
+       this.v -= this.acceleration;
+   } else if(this.game.input.keyboard.isDown(Phaser.Keyboard.D)){
+       this.v += this.acceleration;
+   } else {
+       if(this.v >= -this.maxSpeed && this.v < 0)
+        this.v += this.acceleration;
+       else if(this.v <= this.maxSpeed && this. v > 0)
+        this.v -= this.acceleration;
+   }
+   if(this.v >= this.maxSpeed){
+       this.v = this.maxSpeed;
+   } else if(this.v <= -this.maxSpeed){
+       this.v = -this.maxSpeed;
+   }
+   this.x += this.speed*this.v;
 }
