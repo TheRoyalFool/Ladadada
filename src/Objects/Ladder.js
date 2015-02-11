106415d836626da397ae06e13c5f45895485a2f2
kipppys <themousesack@hotmail.co.uk>
2015-02-11 16:28:07 +0000

    Cleaning

--- src/Objects/Ladder.js
+++ src/Objects/Ladder.js
@@ -1,17 +1,13 @@
-/**
- * Created by William on 07/02/15.
- */
 Ladder = function(game, x, y, img){
     Phaser.Sprite.call(this, game, x, y, img);
     this.game = game;
-    this.type;
 
     game.physics.enable(this, Phaser.Physics.ARCADE);
     this.body.gravity = -game.physics.gravity;
 }
 
 Ladder.prototype = Object.create(Phaser.Sprite.prototype);
-Ladder.prototype.constructor = Item;
+Ladder.prototype.constructor = Ladder;
 
 Ladder.prototype.update = function(){
 
--- src/Objects/Player.js
+++ src/Objects/Player.js
@@ -7,11 +7,10 @@ Player = function(game, x, y, img, speed, jumpHeight) {
     this.jumpHeight = jumpHeight;
     this.dir = 'right';
 
+    //changing players starting health from 0 to 100
     this.health = 100;
 
-    game.load.image('bull', 'assets/bullet');
-
-    //add the player to the physics engine
+    //'this' refers to the player, and this code is adding him to the games physics engine
     game.physics.enable(this, Phaser.Physics.ARCADE);
 
     //give the player some physics values and dont allow him to exit the screen
@@ -21,43 +20,46 @@ Player = function(game, x, y, img, speed, jumpHeight) {
     //sets up sprite for players melee skill
     this.meleeRange = game.add.sprite(this.body.width/2 - 80, y, null);
     game.physics.enable(this.meleeRange);
-    this.meleeRange.body.gravity = -game.physics.gravity;
+    this.meleeRange.body.gravity = -game.physics.gravity; //so the meleeRange body does not fall out of the world
     this.meleeRange.body.setSize(160,64);
-    this.addChild(this.meleeRange);
+    this.addChild(this.meleeRange); //adds melee range to the player DisplayContainer
 
+    //dont know if this is still going to be used
     this.minorAbillityTag = "";
     this.majorAbillityTag = "";
 
+
     //double jump ability variables
-    this.doubleJump = true;
-    this.jumps = 0;
-    this.jumpsTimer = 0;
+    this.doubleJump = true; //this stores if the player has the double jump ability
+    this.jumps = 0; //how many times has the player jumped
+    this.jumpsTimer = 0; //when was the last time the player hit the jump key
 
     //sliding ability variables
-    this.slide = true;
+    this.slide = true; //does the player have the slide ability
     this.canSlide = false;
     this.slideCooldown = 0;
 
-
+    //dont know about this either, still to be completed
     this.majorAbility = game.add.sprite(0, 0, null);
     this.minorAbility = game.add.sprite(0, 0, null);
-
     this.addChild(this.majorAbility);
     this.addChild(this.minorAbility);
+    //till here
 
-    //set up the players gun
+    /*
+     *  When using a variable to store the current player gun it was constantly updating it
+     *  which was bugging the update function. We decided to use this function to update the
+     *  players gun.
+    */
     this.ChangeGun("Hail");
 
-    //variables for movement keys
     this.LeftKey = this.game.input.keyboard.addKey(Phaser.Keyboard.A);
     this.RightKey = this.game.input.keyboard.addKey(Phaser.Keyboard.D);
     this.UpKey = this.game.input.keyboard.addKey(Phaser.Keyboard.W);
     this.DownKey = this.game.input.keyboard.addKey(Phaser.Keyboard.S);
 
+    //variable that stores if the player is currently hitting a ladder
     this.onLadder = false;
-
-
-
 }
 
 Player.prototype = Object.create(Phaser.Sprite.prototype);
@@ -65,44 +67,36 @@ Player.prototype.constructor = Player;
 
 Player.prototype.update = function(){
 
-
-    //player movement and sliding ability
+    /*
+     * Using the players direction call the right slide direction.
+     * This allows the player to dash quickly to one side every 3 seconds
+     * once he double taps one of the movement keys.
+     * Also this handles the players basic movement :)
+     */
     if(this.LeftKey.isDown){
 
         this.dir = 'left';
         this.body.velocity.x = -this.speed;
-
-        //when the player can slide he will slide for 250 millie seconds
         if(this.canSlide == true && this.LeftKey.timeDown+250 > this.game.time.now){
-
-            //reset the slideCooldown variable
             this.slideCooldown  = this.game.time.totalElapsedSeconds() + 3;
-
-            //make the player move 3x faster whilst hes sliding
             this.body.velocity.x = -this.speed*3;
         } else {
-            //when the player has been holding the key for 250 millie seconds reset the canSlide variable
             this.canSlide = false;
         }
 
-
-      //this all does the same as above for the opposite key
     } else if(this.RightKey.isDown){
 
         this.dir = 'right';
         this.body.velocity.x = this.speed;
-
         if(this.canSlide == true && (this.RightKey.timeDown+250) > this.game.time.now){
             this.slideCooldown = this.game.time.totalElapsedSeconds() + 3;
             this.body.velocity.x = this.speed*3;
-
         } else{
             this.canSlide = false;
         }
 
     } else {
 
-        //when the player is not pressing any movement key then stop the player
         this.body.velocity.x = 0;
 
     }
@@ -120,44 +114,15 @@ Player.prototype.update = function(){
         }
     }
 
-    //this keeps the canSlide variable false while the slideCooldown is active
-    if(this.slideCooldown > this.game.time.totalElapsedSeconds() && this.RightKey.isUp && this.dir == 'right' ||
-        this.slideCooldown > this.game.time.totalElapsedSeconds() && this.LeftKey.isUp && this.dir == 'left'){
-        this.canSlide = false;
-    }
+    //handles everything about sliding
+    SlidingUpdate();
 
-    //whilst the slide abillity variable is active and the slideCooldown is not active
-    if(this.slide == true && this.slideCooldown < this.game.time.totalElapsedSeconds()){
-
-        /*
-            These two statements are timed to check 2 separate key events
-            - KeyDown
-            - KeyUp
-            the player has 100 millie seconds to hit the right of left key twice
-            this code paired with the movement code above allows the player to slide.
-        */
-
-        if((this.RightKey.timeDown+100) > this.game.time.now){
-            if((this.RightKey.timeUp+100) > this.game.time.now){
-                console.log("can slide");
-                this.canSlide = true;
-            }
-        }
-        if(this.LeftKey.timeDown+100 > this.game.time.now){
-            if(this.LeftKey.timeUp+100 > this.game.time.now){
-                console.log("can slide");
-                this.canSlide = true;
-            }
-        }
-    }
-
-
+    //This allows the player to jump for a second time whilst in the air
     if(this.doubleJump == true && this.jumps < 2 && this.jumpsTimer <= this.game.time.now){
         if(this.game.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR) ||
             this.game.input.keyboard.isDown(Phaser.Keyboard.W)){
             this.body.velocity.y = -this.jumpHeight;
             this.jumps = 2;
-            console.log("2 jumps");
         }
     }
 
@@ -174,7 +139,6 @@ Player.prototype.update = function(){
         this.jumps = 0;
     }
 
-    //player crouching
     if(this.game.input.keyboard.isDown(Phaser.Keyboard.S)){
         this.body.setSize(64 ,32 , 0, 32);
         this.loadTexture('playercrouched');
@@ -183,10 +147,7 @@ Player.prototype.update = function(){
         this.loadTexture(this.playerImg);
     }
 
-    //cap the falling speed of the player
-    if(this.body.velocity.y > 1000){
-        this.body.velocity.y = 1000;
-    }
+    this.body.maxVelocity.y = 1000;
 
     //update the players gun
     this.playerGun.update(this.x + this.body.width / 2,  this.y + this.body.height / 2);
@@ -249,5 +210,41 @@ Player.prototype.AbilityBehaviours = function(){
     if(this.slide === true){
 
     }
+}
+
+
+
+function SlidingUpdate(){
+    //this keeps the canSlide variable false while the slideCooldown is active
+    if(this.slideCooldown > this.game.time.totalElapsedSeconds() && this.RightKey.isUp && this.dir == 'right' ||
+        this.slideCooldown > this.game.time.totalElapsedSeconds() && this.LeftKey.isUp && this.dir == 'left'){
+        this.canSlide = false;
+    }
+
+    //whilst the slide abillity variable is active and the slideCooldown is not active
+    if(this.slide == true && this.slideCooldown < this.game.time.totalElapsedSeconds()){
+
+        /*
+         These two statements are timed to check 2 separate key events
+         - KeyDown
+         - KeyUp
+         the player has 100 millie seconds to hit the right of left key twice
+         this code paired with the movement code above allows the player to slide.
+         */
+
+        if((this.RightKey.timeDown+100) > this.game.time.now){
+            if((this.RightKey.timeUp+100) > this.game.time.now){
+                console.log("can slide");
+                this.canSlide = true;
+            }
+        }
+        if(this.LeftKey.timeDown+100 > this.game.time.now){
+            if(this.LeftKey.timeUp+100 > this.game.time.now){
+                console.log("can slide");
+                this.canSlide = true;
+            }
+        }
+    }
+
 }
 
